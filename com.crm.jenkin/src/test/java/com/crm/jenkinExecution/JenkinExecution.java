8aba3519e6f8988171f9f5cceaabc192f6666174
Sony <Sony@DESKTOP-O62RJ5V>
2022-03-28 13:03:29 +0530

    commit one

---
+++ com.crm.jenkin/src/test/java/com/crm/jenkinExecution/JenkinExecution.java
@@ -0,0 +1,18 @@
+package com.crm.jenkinExecution;
+
+import org.testng.annotations.Test;
+
+public class JenkinExecution {
+
+	@Test
+	public void jenkin() {
+		
+		System.out.println("start execution");
+	}
+	
+	@Test
+	public void jenkinExecution()
+	{
+		System.out.println("end execution");
+	}
+}
